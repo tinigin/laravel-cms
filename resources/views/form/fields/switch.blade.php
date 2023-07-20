7defb90f05c9adc25b8493d179b15cdb245b31ad
Dmitrii Tinigin <tinigin@gmail.com>
2023-07-20 10:58:03 +0300

    Update switch.blade.php

--- resources/views/form/fields/switch.blade.php
+++ resources/views/form/fields/switch.blade.php
@@ -1,12 +1,7 @@
 @component($typeForm, get_defined_vars())
-    @isset($sendTrueOrFalse)
-        <input hidden name="{{$attributes['name']}}" value="{{$attributes['novalue']}}">
+    @if($attributes['class'] == 'form-check-input')
         <div class="form-check form-switch">
-            <input value="{{$attributes['yesvalue']}}"
-                   {{ $attributes }}
-                   @if(isset($attributes['value']) && $attributes['value']) checked @endif
-                   id="{{$id}}"
-            >
+            <input {{ $attributes }} role="switch" id="{{$id}}" value="1" @if($value) checked @endif>
             <label class="form-check-label" for="{{$id}}">{{$placeholder ?? ''}}</label>
         </div>
     @else
