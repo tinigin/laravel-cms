ebab528e21ea1fc582d187e5f03d03c42a620756
Dmitrii Tinigin <tinigin@gmail.com>
2023-11-20 14:27:23 +0300

    Update toast.blade.php

--- resources/views/partials/toast.blade.php
+++ resources/views/partials/toast.blade.php
@@ -1,7 +1,8 @@
 @if (session()->has(\LaravelCms\Alert\Toast::SESSION_MESSAGE))
+    @php ($level = session(\LaravelCms\Alert\Toast::SESSION_LEVEL))
     <div
         class="toast-message hidden"
-        data-icon="{{ session(\LaravelCms\Alert\Toast::SESSION_LEVEL) }}"
+        data-icon="{{ $level == 'danger' ? 'error' : $level }}"
         data-title="{{ session(\LaravelCms\Alert\Toast::SESSION_MESSAGE_TITLE) }}"
     >{{ session(\LaravelCms\Alert\Toast::SESSION_MESSAGE) }}</div>
 @endif
