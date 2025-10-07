d041ec65c17ab1c8feffb2bf4c948a7b78a4b484
tinigin <tinigin@gmail.com>
2025-10-07 15:37:32 +0300

    readonly mode for switch form element

--- resources/views/form/fields/switch.blade.php
+++ resources/views/form/fields/switch.blade.php
@@ -1,12 +1,12 @@
 @component($typeForm, get_defined_vars())
     @if($attributes['class'] == 'form-check-input')
         <div class="form-check form-switch">
-            <input {{ $attributes }} role="switch" id="{{$id}}" value="1" @if($value) checked @endif>
+            <input {{ $attributes }} role="switch" id="{{$id}}" value="1" @if($value) checked @endif {{$readonly ? 'disabled="true"' : ''}}>
             <label class="form-check-label" for="{{$id}}">{{$placeholder ?? ''}}</label>
         </div>
     @else
         <div class="custom-control custom-switch">
-			<input {{ $attributes }} id="{{$id}}" value="1" @if($value) checked @endif>
+			<input {{ $attributes }} id="{{$id}}" value="1" @if($value) checked @endif {{$readonly ? 'disabled="true"' : ''}}>
 			<label class="custom-control-label" for="{{$id}}">{{$placeholder ?? ''}}</label>
 		</div>
     @endisset
